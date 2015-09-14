3b924a0ceb6e8561652df17a8b1804237ca7a19f
vaberay <ramon@wishpond.com>
2015-09-14 09:59:08 -0700

    Survey gizmo omniauth

---
+++ lib/omniauth/surveygizmo.rb
@@ -0,0 +1,15 @@
+require "omniauth/surveygizmo/version"
+
+module OmniAuth
+  module Strategies
+    class Surveygizmo < OmniAuth::Strategies::OAuth
+
+      option :client_options, {
+        :site       => "http://restapi.surveygizmo.com",
+        :request_token_path => "/head/oauth/request_token",
+        :access_token_path => "/head/oauth/access_token",
+        :authorize_url => "http://restapi.surveygizmo.com/head/oauth/authenticate"
+      }
+    end
+  end
+end
---
+++ lib/omniauth/surveygizmo/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Surveygizmo
+    VERSION = "0.1.0"
+  end
+end
